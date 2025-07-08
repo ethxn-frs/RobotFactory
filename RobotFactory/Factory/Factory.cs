363505f33dbdba4a0f62b6460245f0e041389a82
Aurelien <aurebtz@gmail.com>
2025-07-08 12:48:34 +0200

    add WITH, WITHOUT, REPLACE. Parseur étendu + Validation des contraintes de catégorie (D, I, M).

--- RobotFactory/Factory/Factory.cs
+++ RobotFactory/Factory/Factory.cs
@@ -14,20 +14,21 @@ namespace RobotFactory.Factory
             return name switch
             {
                 "XM-1" => new Robot("XM-1", new List<string>
-            {
-                "Core_CM1", "Generator_GM1", "Arms_AM1", "Legs_LM1", "System_SB1"
-            }),
+                {
+                    "Core_CM1", "Generator_GM1", "Arms_AM1", "Legs_LM1", "System_SB1"
+                }, "M"),
                 "RD-1" => new Robot("RD-1", new List<string>
-            {
+                {
                 "Core_CD1", "Generator_GD1", "Arms_AD1", "Legs_LD1", "System_SB1"
-            }),
-                "WI-1" => new Robot("WI-1", new List<string>
-            {
+                }, "D"),
+                        "WI-1" => new Robot("WI-1", new List<string>
+                {
                 "Core_CI1", "Generator_GI1", "Arms_AI1", "Legs_LI1", "System_SB1"
-            }),
+                }, "I"),
                 _ => throw new ArgumentException($"Robot inconnu : {name}")
             };
         }
+
     }
 
 }
--- RobotFactory/Models/Robot.cs
+++ RobotFactory/Models/Robot.cs
@@ -4,10 +4,12 @@ public class Robot
 {
     public string Name { get; set; }
     public List<string> RequiredPieces { get; set; }
+    public string Category { get; set; }
 
-    public Robot(string name, List<string> requiredPieces)
+    public Robot(string name, List<string> requiredPieces, string category)
     {
         Name = name;
         RequiredPieces = requiredPieces;
+        Category = category;
     }
 }
